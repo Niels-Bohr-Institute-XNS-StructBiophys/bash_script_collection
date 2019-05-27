ab4a1f3d88315bc268a54c41296ef7a16f5b6fe9
Martin Cramer Pedersen <mcpe@nbi.dk>
2019-05-27 20:42:31 +0200

    updated export_fit2d_image.sh with support of background subtr and choose of linlog scale and better filename convention for output; updated corresponding *.mac as well.

--- export_fit2d_image.sh
+++ export_fit2d_image.sh
@@ -3,9 +3,10 @@
 # use -i flag here to use aliases like fit2d
 #
 # e.g.
-# export_fit2d_image latest_002217[0-8]*.tiff 40 60 280 197 50 250 mask.msk
-# export_fit2d_image latest_002217[0-8]*.tiff 40 60 280 197 50 250 none
-# ls latest_002217[0-8]*.tiff | export_fit2d_image - 40 60 280 197 50 250 mask.msk for pipes
+# export_fit2d_image im_002217[0-8]_caz.tiff 40 60 280 197 50 250 log mask.msk im_0022169_caz.tiff
+# export_fit2d_image im_002217[0-8]*.tiff 40 60 280 197 50 250 lin none none
+# for pipes (e.g. with ls):
+# ls im_002217[0-8]*.tiff | export_fit2d_image - 40 60 280 197 50 250 log mask.msk none
 
 
 # fit2d alias must be set, too
@@ -13,25 +14,28 @@
 path_to_mac='/home/msmile/Seafile/MartinS/bash/'
 
 
-# at least 6 input args are required (file(s) rectangle corners and mask)
-if (( "$#" < 8 )); then
-	echo "At least 8 input arguments needed!"
+# at least 10 input args are required (file(s) rectangle corners and mask)
+if (( "$#" < 10 )); then
+	echo "At least 10 input arguments needed!"
 	echo ""
 	echo "Usage (using wildcards and pipes possible):"
 	echo ""
 	echo "With mask-file:"
-	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <mask-file>"
+	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> <mask-file> <background-file>"
 	echo ""
-	echo "Without mask-file use none:"
-	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> none"
+	echo "Without mask-file and background-file use none each:"
+	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> none none"
 	echo ""
-	echo "Without mask-file and fully automatic intensity scale:"
-	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> auto auto none"
+	echo "Without mask-file and background-file and fully automatic intensity scale:"
+	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> auto auto <linlog> none none"
 	echo ""
 	echo "Same with pipes:"
-	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <mask-file>"
-	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> none"
-
+	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> <mask-file> <background-file>"
+	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> none none"
+	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> ... "
+	echo ""
+	echo "<linlog>: log or lin for logarithmic or linear intensity scale"
+	echo ""
 	exit
 fi
 
@@ -39,7 +43,7 @@ fi
 # read files into filelist
 filelist=(" ")
 i=0
-while test $# -gt 7; do
+while test $# -gt 9; do
 	filelist[i]=$1
 	shift
 	i=$(($i + 1))
@@ -57,11 +61,43 @@ fi
 #echo "${filelist[@]}"
 
 
-# now all files are in filelist and arguments are shifted such that there only 7 args left
-if [ "$7" != "none" ] && [ ! -f $7 ] ; then
+# now all files are in filelist and arguments are shifted, we have $1...$9
+# list all args
+i=1
+for j in $*; do
+	echo "$i: $j"
+	i=$(($i + 1))
+done
+
+# intensity scale
+if [ "$7" == "lin" ]; then
+	scale="LINEAR SCALE"
+elif [ "$7" == "log" ]; then
+	scale="LOG SCALE"
+else
+	echo "Error: <linlog> argument is $7. It must be either lin or log. Exit."
+	exit
+fi
+
+# mask
+if [ "$8" != "none" ] && [ ! -f $8 ] ; then
 	echo "Error: mask-file not found. Exit."
 	exit
 fi
+
+# background subtraction
+if [ "$9" != "none" ] && [ ! -f $9 ] ; then
+	echo "Error: background-file not found. Exit."
+	exit
+fi
+
+if [ "$9" == "none" ] ; then
+	subtr=""
+else
+	subtr=${9/".tiff"/""}
+	subtr="-$subtr"
+fi
+echo "subtr=$subtr"
 
 
 for file in ${filelist[@]}
@@ -72,26 +108,43 @@ do
 	continue
 	fi
 
-	image=${file/".tiff"/".ps"}
+	image=${file/".tiff"/""}
+	image="${image}${subtr}_$1_$2_$3_$4_$5_$6_$7_$8.ps"
+	echo "image=$image"
 
 	# call fit2d macro
-
 	if [ "$5" == "auto" ]; then
-		if [ "$7" == "none" ]; then
-		$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#FILE_IN=$file -svar#FILE_OUT=$image -mac${path_to_mac}fit2d_image_nomask_autoscale.mac)
+		if [ "$8" == "none" ]; then
+			if [ "$9" == "none" ]; then
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -mac${path_to_mac}fit2d_image_nomask_nosubtr_autoscale.mac)
+			else
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#SUBTR=$9 -mac${path_to_mac}fit2d_image_nomask_withsubtr_autoscale.mac)
+			fi
 		else
-		$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$7 -mac${path_to_mac}fit2d_image_withmask_autoscale.mac)
+			if [ "$9" == "none" ]; then
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -mac${path_to_mac}fit2d_image_withmask_nosubtr_autoscale.mac)
+			else
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -svar#SUBTR=$9 -mac${path_to_mac}fit2d_image_withmask_withsubtr_autoscale.mac)
+			fi
 		fi
 	else
-		if [ "$7" == "none" ]; then
-		$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#FILE_IN=$file -svar#FILE_OUT=$image -mac${path_to_mac}fit2d_image_nomask.mac)
+		if [ "$8" == "none" ]; then
+			if [ "$9" == "none" ]; then
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -mac${path_to_mac}fit2d_image_nomask_nosubtr.mac)
+			else
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#SUBTR=$9 -mac${path_to_mac}fit2d_image_nomask_withsubtr.mac)
+			fi
 		else
-		$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$7 -mac${path_to_mac}fit2d_image_withmask.mac)
+			if [ "$9" == "none" ]; then
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -mac${path_to_mac}fit2d_image_withmask_nosubtr.mac)
+			else
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -svar#SUBTR=$9 -mac${path_to_mac}fit2d_image_withmask_withsubtr.mac)
+			fi
 		fi
 	fi
 
-
-
-	# use ps2pdf + pdfcrop + convert to png  
-	ps2pdfcrop2pngbatch -r $image
+	ps2pdf $image ${image/".ps"/".pdf"}
+	pdfcrop ${image/".ps"/".pdf"} ${image/".ps"/"_cropped.pdf"}
+	convert -density 300 -quality 100 ${image/".ps"/"_cropped.pdf"} ${image/".ps"/".png"}
+	rm $image ${image/".ps"/".pdf"} ${image/".ps"/"_cropped.pdf"}
 done
