0b2ba4c2098c4f6e85ea9210bd090647e9063df4
p3scmr <martin.schmiele@nbi.ku.dk>
2020-03-04 10:33:05 +0100

    updated export_fit2d_image.sh

--- export_fit2d_image.sh
+++ export_fit2d_image.sh
@@ -15,23 +15,25 @@ path_to_mac='/home/msmile/Seafile/MartinS/bash/'
 
 
 # at least 10 input args are required (file(s) rectangle corners and mask)
-if (( "$#" < 10 )); then
-	echo "At least 10 input arguments needed!"
+if (( "$#" < 13 )); then
+	echo "At least 13 input arguments needed (including one sample file)!"
+	echo "Background subtraction with vol_fract applies to all sample files. CF_sample applies to all sample files."
+	echo "Intensity auto scaling must apply to both, Imax and Imin."
 	echo ""
 	echo "Usage (using wildcards and pipes possible):"
 	echo ""
-	echo "With mask-file:"
-	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> <mask-file> <background-file>"
+	echo "With mask-file and background subtraction:"
+	echo "export_fit2d_image <sample-file(s)> <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> <mask-file> <background-file> <vol_fract> <CF_sample> <CF_background>"
 	echo ""
-	echo "Without mask-file and background-file use none each:"
-	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> none none"
+	echo "Without mask-file and without background-file use none each:"
+	echo "export_fit2d_image <sample-file(s)> <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> none none 0.0 1.0 0.0"
 	echo ""
 	echo "Without mask-file and background-file and fully automatic intensity scale:"
-	echo "export_fit2d_image <tiff-file(s)> <xmin> <ymin> <xmax> <ymax> auto auto <linlog> none none"
+	echo "export_fit2d_image <sample-file(s)> <xmin> <ymin> <xmax> <ymax> auto auto <linlog> none none 0.0 1.0 0.0"
 	echo ""
 	echo "Same with pipes:"
-	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> <mask-file> <background-file>"
-	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> none none"
+	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> <mask-file> <background-file> <vol_fract> <CF_sample> <CF_background>"
+	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> <Imin> <Imax> <linlog> none none 0.0 1.0 0.0"
 	echo "... | export_fit2d_image - <xmin> <ymin> <xmax> <ymax> ... "
 	echo ""
 	echo "<linlog>: log or lin for logarithmic or linear intensity scale"
@@ -43,7 +45,7 @@ fi
 # read files into filelist
 filelist=(" ")
 i=0
-while test $# -gt 9; do
+while test $# -gt 12; do
 	filelist[i]=$1
 	shift
 	i=$(($i + 1))
@@ -61,7 +63,7 @@ fi
 #echo "${filelist[@]}"
 
 
-# now all files are in filelist and arguments are shifted, we have $1...$9
+# now all files are in filelist and arguments are shifted, we have $1...$12
 # list all args
 i=1
 for j in $*; do
@@ -100,6 +102,10 @@ fi
 echo "subtr=$subtr"
 
 
+volfract=$10
+CFsample=$11
+CFbackgr=$12
+
 for file in ${filelist[@]}
 do
 #	printf "$file: "
@@ -117,36 +123,36 @@ do
 	mask=${mask/".msk"/""}
 	#echo $mask
 
-	image="${image}${subtr}_$1_$2_$3_$4_$5_$6_$7_$mask.ps"
+	image="${image}${subtr}_$1_$2_$3_$4_$7_$mask.ps"
 	echo "image=$image"
 
 	# call fit2d macro
-	if [ "$5" == "auto" ]; then
-		if [ "$8" == "none" ]; then
-			if [ "$9" == "none" ]; then
+	if [ "$5" == "auto" ]; then # autoscale
+		if [ "$8" == "none" ]; then # no mask
+			if [ "$9" == "none" ]; then # no subtraction
 				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -mac${path_to_mac}fit2d_image_nomask_nosubtr_autoscale.mac)
-			else
-				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#SUBTR=$9 -mac${path_to_mac}fit2d_image_nomask_withsubtr_autoscale.mac)
+			else # with subtraction
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#SUBTR=$9 -fvar#VOL_FRACT=$10 -fvar#CF_SAMPLE=$11 -fvar#CF_BACKGR=$12 -mac${path_to_mac}fit2d_image_nomask_withsubtr_autoscale.mac)
 			fi
-		else
-			if [ "$9" == "none" ]; then
+		else # with mask
+			if [ "$9" == "none" ]; then # no subtraction
 				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -mac${path_to_mac}fit2d_image_withmask_nosubtr_autoscale.mac)
-			else
-				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -svar#SUBTR=$9 -mac${path_to_mac}fit2d_image_withmask_withsubtr_autoscale.mac)
+			else # with subtraction
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -svar#SUBTR=$9 -fvar#VOL_FRACT=$10 -fvar#CF_SAMPLE=$11 -fvar#CF_BACKGR=$12 -mac${path_to_mac}fit2d_image_withmask_withsubtr_autoscale.mac)
 			fi
-		fi
-	else
-		if [ "$8" == "none" ]; then
-			if [ "$9" == "none" ]; then
+		fi 
+	else # with autoscale
+		if [ "$8" == "none" ]; then # no mask
+			if [ "$9" == "none" ]; then # no subtraction
 				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -mac${path_to_mac}fit2d_image_nomask_nosubtr.mac)
-			else
-				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#SUBTR=$9 -mac${path_to_mac}fit2d_image_nomask_withsubtr.mac)
+			else # with subtraction
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#SUBTR=$9 -fvar#VOL_FRACT=$10 -fvar#CF_SAMPLE=$11 -fvar#CF_BACKGR=$12 -mac${path_to_mac}fit2d_image_nomask_withsubtr.mac)
 			fi
-		else
-			if [ "$9" == "none" ]; then
+		else # with mask
+			if [ "$9" == "none" ]; then # no subtraction
 				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -mac${path_to_mac}fit2d_image_withmask_nosubtr.mac)
-			else
-				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -svar#SUBTR=$9 -mac${path_to_mac}fit2d_image_withmask_withsubtr.mac)
+			else # with subtraction
+				$(fit2d -key -dim1024x1024 -fvar#XMIN=$1 -fvar#YMIN=$2 -fvar#XMAX=$3 -fvar#YMAX=$4 -fvar#MIN=$5 -fvar#MAX=$6 -svar#SCALE=$scale -svar#FILE_IN=$file -svar#FILE_OUT=$image -svar#MASK=$8 -svar#SUBTR=$9 -fvar#VOL_FRACT=$10 -fvar#CF_SAMPLE=$11 -fvar#CF_BACKGR=$12 -mac${path_to_mac}fit2d_image_withmask_withsubtr.mac)
 			fi
 		fi
 	fi
